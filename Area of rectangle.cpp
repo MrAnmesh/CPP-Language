43210807da479dfd64e61b71cadefb6d27ca0d89
Anmesh Mishra <108453134+MrAnmesh@users.noreply.github.com>
2023-08-01 16:56:26 +0530

    Add files via upload

---
+++ Area of rectangle.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a,b,c;
+ 	cout<<"enter side of rectangle: ";
+ 	cin>>a; 
+ 	cin>>b;
+ 	c = a*b;
+ 	cout<<"area of rectangle: "<<c;
+}
