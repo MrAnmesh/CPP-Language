ebfc6c1f7f389e3ac71beaebd91ef69d55187bcd
Anmesh Mishra <108453134+MrAnmesh@users.noreply.github.com>
2023-08-01 17:07:00 +0530

    Add files via upload

---
+++ percentage conversion.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	float a,percentage;
+ 	cout<<"enter marks between 0 to 80: ";
+ 	cin>>a;
+ 	percentage = (a/80.00)*100.00;
+	 
+	cout<<"requiered percentage: "<<percentage;
+}
