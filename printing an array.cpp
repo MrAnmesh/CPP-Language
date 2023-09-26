a566b38983084ad46ce9acb1d00950119c9fbea1
Anmesh Mishra <108453134+MrAnmesh@users.noreply.github.com>
2023-09-26 21:13:47 +0530

    Add files via upload

---
+++ printing an array.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int arr[5]={1,2,3,4,5};
+	
+// printing this array:-
+
+	int n=5;
+	for(int i =0; i<=n; i++)
+	{
+		cout<<arr[i]<<endl;
+	}
+}
+
+
